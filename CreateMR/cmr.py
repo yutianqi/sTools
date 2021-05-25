5ce6e7c5b20e016957485687d4935c1468f3dbc9
y00290641 <yutianqi@huawei.com>
2021-05-25 20:05:26 +0800

    add cmd

---
+++ CreateMR/cmr.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import sys
+import os
+import git
+
+
+CHROME_PATH=r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+CODE_DIR=r'D:\NetEco\01.code'
+REPO_URL= 'https://codehub-y.huawei.com/y00290641'
+
+
+
+def main():
+    pwd = os.getcwd()
+    # pwd = r'D:\NetEco\01.code\NetEcoBasicService\base'
+    
+    field = pwd.split('\\')
+   
+    basicDir = '\\'.join(field[0:4])
+    # print('执行目录是:' + basicDir)
+    
+    projectName=field[3]
+    # print('工程是:' + projectName)
+    
+    repo = git.Repo(basicDir)
+    sourceBranch = str(repo.active_branch)
+    targetBranch = str(repo.active_branch)
+    # print('分支是:' + sourceBranch)
+
+    cmd = '"' + CHROME_PATH + '" ' + REPO_URL + '/' + projectName + '/' + 'newmergeform?source_branch=' + sourceBranch # + '&target_branch=' + targetBranch
+    # print(cmd)
+    os.system(r"" + cmd + "")
+
+if __name__ == '__main__': 
+    main()
+
